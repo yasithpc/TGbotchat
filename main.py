23ae2b38f0036ddf6f64f1be1e3e6207aa4f826f
yasithpc <83853349+yasithpc@users.noreply.github.com>
2021-08-03 12:46:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import telebot
+
+
+bot = telebot.TeleBot("1914396081:AAHNFA2409oexT-JMVr5eOnkFMaXb__3xkg")
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+  bot.reply_to(message, "Hello! I'm BST Community Chat Bot")
+
+
+@bot.message_handler(commands=["bottCreator"])
+def send_message(message):
+  bot.send_message(message, "yasith")
+
+
+
+bot.polling()
