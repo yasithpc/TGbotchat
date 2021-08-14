980d06a397fc612784160b2dc71562b802d70a60
yasithpc <83853349+yasithpc@users.noreply.github.com>
2021-08-14 22:15:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def send_message(message):
     
 def handle_messages(messages):
 	for message in messages:
-		if messages == "hi" or messages == "Hi":
+		if message.chat.type == "hi" or message.chat.type == "Hi":
 		# Do something with the message
 			bot.reply_to(message, 'Hi')
 
