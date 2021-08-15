9c7dcafac811834b5bfbc54826fd58fce2a891ea
yasithpc <83853349+yasithpc@users.noreply.github.com>
2021-08-15 10:38:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot("1914396081:AAHNFA2409oexT-JMVr5eOnkFMaXb__3xkg")
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
   bot.reply_to(message, """
-  Hello! I'm BST Community Chat Bot.
+  Hello!😊 I'm BST Community Chat Bot.💖
   
   my command list :
   
